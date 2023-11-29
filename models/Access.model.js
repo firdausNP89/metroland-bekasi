f7fc315ccc5ee4d2a87e3373692592ce764fe364
firdaus nanda putra <firdaus.nandaputra@ebconnect.com>
2023-11-29 23:28:34 +0700

    add models

---
+++ models/Access.model.js
@@ -0,0 +1,45 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = model;
+
+function model(sequelize) {
+    const access = {
+        title: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        image: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        created_by: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        updated_by: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        deleted_by: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    };
+
+    const options = {
+        tableName: "access",
+        createdAt: "created_at",
+        updatedAt: "updated_at",
+        deletedAt: "deleted_at",
+        paranoid: true,
+        defaultScope: {
+            attributes: {}
+        }
+    };
+
+    return sequelize.define('access', access, options);
+}
